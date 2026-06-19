603482b5706bd0c63a1cf37629968beb172be22a
puspus-dev <boszormenyiadam03@gmail.com>
2026-06-19 11:42:01 +0000

    Extension github enable

--- src/lib/pages/Downloads.svelte
+++ src/lib/pages/Downloads.svelte
@@ -194,7 +194,7 @@
 			<div class="card-section">
 				<p class="font_header_14px">Forráskód</p>
 				<div class="section-buttons">
-					<Buttons label="GitHub" href={githubLinks.extension} icon="ri:github-fill" disabled="true"></Buttons>
+					<Buttons label="GitHub" href={githubLinks.extension} icon="ri:github-fill" disabled="false"></Buttons>
 				</div>
 			</div>
 		</div>
